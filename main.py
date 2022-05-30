0c3a78a42e681dc5cc2cfe6bdf3e4019e6b76e08
AndronovaAA <43322190+AndronovaAA@users.noreply.github.com>
2022-05-31 01:34:42 +0300

    added observer for state feedback case

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from State_feedback_LMI import state_feedback_controller
 from Check_stability import check_output_feedback, check_state_feedback,check_observer
 from Observers import observer_output
 
+#output feedback(decomposition+LMI+observer)
 #x_dot = (A+F1*Delta*E1)x + (B+F1*Delta*E2)u + J*lambda
 #G*x_dot = 0
 #y = Cx
@@ -42,7 +43,46 @@ L = observer_output(system_N)
 stab_ob = check_observer(system_N, L)
 print("")
 
+##############################
+#state feedback(decomposition+LMI+observer)
+#x_dot = (A+F1*Delta*E1)x + (B+F1*Delta*E2)u + J*lambda
+#G*x_dot = 0
+#y=Cx
+
+# Making Random matrices without F2
+size_x = 6
+size_y = 6
+size_u = 6
+size_l = 4
+A, B, C, G, F_1, F_2, E_1, E_2 = Random_System(size_x,size_y,size_u,size_l, F2 = False)
+
+# Determining other parameters
+mytol = 0.000001
+ObserverCost   = {'Q': 100*np.eye(size_l), 'R': 100*np.eye(size_y)}
+
+# Initializing system
+system = System(A, B, C, G, F_1, F_2, E_1, E_2, ObserverCost, mytol)
+
+#decomposition
+system_N = decomposition(system)
+
+# Getting control output feedback for system with decomposition
+# for the system with decomposition returns K*N
+K = state_feedback_controller(system_N)
+
+#Number of deltas to check
+num_d = 1000
+#
+#Checking eigenvalues
+stab = check_state_feedback(system_N, K, num_d)
+print("% of stable systems(output feedback + decomposition):", stab)
+
+L = observer_output(system_N)
+stab_ob = check_observer(system_N, L)
+print("")
+
 ##########
+#state feedback(LMI)
 #x_dot = (A+F1*Delta*E1)x + (B+F1*Delta*E2)u
 # Making Random matrices without F2
 size_x = 6
@@ -53,7 +93,7 @@ A, B, C, G, F_1, F_2, E_1, E_2 = Random_System(size_x,size_y,size_u,size_l, F2 =
 
 # Determining other parameters
 mytol = 0.000001
-ObserverCost   = {'Q': 100*np.eye(size_l), 'R': np.array([[C.shape[0]]])}
+ObserverCost   = {'Q': 100*np.eye(size_l), 'R': 100*np.eye(size_y)}
 
 # Initializing system
 system2 = System(A, B, C, G, F_1, F_2, E_1, E_2, ObserverCost, mytol)
@@ -71,6 +111,7 @@ print("% of stable systems(state feedback):", stab2)
 print("")
 
 ##########
+#output feedback case(LMI)
 #x_dot = (A+F1*Delta*E1)x + (B+F1*Delta*E2)u
 #y = (C+F2*Delta*E2)x
 # Making Random matrices
@@ -82,7 +123,7 @@ A, B, C, G, F_1, F_2, E_1, E_2 = Random_System(size_x,size_y,size_u,size_l, F2 =
 
 # Determining other parameters
 mytol = 0.000001
-ObserverCost   = {'Q': 100*np.eye(size_l), 'R': np.array([[C.shape[0]]])}
+ObserverCost   = {'Q': 100*np.eye(size_l), 'R': 100*np.eye(size_y)}
 
 # Initializing system
 system3 = System(A, B, C, G, F_1, F_2, E_1, E_2, ObserverCost, mytol)
@@ -99,6 +140,7 @@ stab3 = check_output_feedback(system3, A_K3, B_K3, C_K3,D_K3, num_d)
 print("% of stable systems(output feedback):", stab3)
 print("")
 #######
+#state feedback controller(decomposition+LMI)
 #x_dot = (A+F1*Delta*E1)x + (B+F1*Delta*E2)u + J*lambda
 #G*x_dot = 0
 
@@ -111,7 +153,7 @@ A, B, C, G, F_1, F_2, E_1, E_2 = Random_System(size_x,size_y,size_u,size_l, F2 =
 
 # Determining other parameters
 mytol = 0.000001
-ObserverCost   = {'Q': 100*np.eye(size_l), 'R': np.array([[C.shape[0]]])}
+ObserverCost   = {'Q': 100*np.eye(size_l), 'R': 100*np.eye(size_y)}
 
 # Initializing system
 system = System(A, B, C, G, F_1, F_2, E_1, E_2, ObserverCost, mytol)
@@ -130,3 +172,4 @@ num_d = 1000
 stab = check_state_feedback(system_N, K, num_d)
 print("% of stable systems(output feedback + decomposition):", stab)
 print("")
+
